1acaeb67434d45c1e866b83bfd0adb3ab27a3eaf
Michael Lyon <lyoncodes@gmail.com>
2019-03-07 22:53:16 -0800

    adds pg database,get routes for search page

--- server.js
+++ server.js
@@ -2,22 +2,29 @@
 // import dependencies
 require('dotenv').config()
 const express = require('express')
-
+const pg = require('pg')
 
 // app 
 const app = express() 
 
 // PORT
 const PORT = process.env.PORT || 3000
-
 app.use(express.static('./public'))
+
+// Database Setup
+const client = new pg.Client(process.env.DATABASE_URL)
+client.connect()
+client.on('error', err => console.error(err))
+
 // Set the view engine
 app.set('view engine', 'ejs')
 
 app.get('/home', home)
+app.get('/nba', nba)
 app.post('/resume', resume)
 app.post('/CVV', cvv)
 app.post('/nba', nba)
+// app.post('/save', saveGuest)
 
 function home(req, res) {
  res.render('index')
@@ -31,6 +38,10 @@ function cvv(req, res) {
 function nba(req, res) {
  res.render('nba')
 }
+
+// function saveGuest(req, res) {
+
+// }
 app.listen(PORT, () => console.log(`listening on port ${3000}`))
 
 // Error handling
