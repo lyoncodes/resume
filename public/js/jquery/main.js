9fdd5445c5a09772754afbbde3a6f3baf890b5fe
Michael Lyon <lyoncodes@gmail.com>
2019-09-22 20:31:14 -0700

    adds links to pages, adds favorites section

--- public/js/jquery/main.js
+++ public/js/jquery/main.js
@@ -1,5 +1,7 @@
 'use strict'
 $(document).ready(() => {
+
+  // sky scraper scroll functions
   $(".apt-body").on('click', (event) => {
     console.log('clicked');
     if ("#amenities" !== "") {
@@ -47,6 +49,7 @@ $(document).ready(() => {
     }
   })
 
+// nav menu scroll downs
   $("#target1").on('click', (event) => {
     $("#hash1").toggle(600);
     $(".nav-menu-ul").scrollTop = $(".nav-menu-ul").scrollHeight;
@@ -59,6 +62,36 @@ $(document).ready(() => {
     $("#hash3").toggle(600);
     $(".nav-menu-ul").scrollTop = $(".nav-menu-ul").scrollHeight;
   })
+
+// nav menu image labels
+ $("#box1").on('hover', (event)=>{
+   $("#label1").toggle(100);
+ })
+ $("#box2").on('hover', (event)=>{
+  $("#label2").toggle(100);
+ })
+ $("#box3").on('hover', (event)=>{
+  $("#label3").toggle(100);
+ })
+ $("#box4").on('hover', (event)=>{
+  $("#label4").toggle(100);
+ })
+ $("#box5").on('hover', (event)=>{
+  $("#label5").toggle(100);
+ })
+
+ //main-page section display
+ $("#favorites-link").on('click', (e) => {
+   $('.display-on-click').toggle(800);
+    if (".display-on-click" !== ""){
+      e.preventDefault();
+      let hash = "#favorites-display"
+      $("html, body").animate({
+        scrollTop: $(hash).offset().top }, 800, () => {
+          window.location.hash = hash;
+      })
+    }
+ });
 
 });
 
