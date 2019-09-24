e944e1c501414f9d31171fd0f7302aaf62fa64fd
Michael Lyon <lyoncodes@gmail.com>
2019-09-23 18:37:14 -0700

    parses jquery, adds scroll function to spa pages

--- public/js/jquery/main.js
+++ public/js/jquery/main.js
@@ -1,54 +1,6 @@
 'use strict'
 $(document).ready(() => {
 
-  // sky scraper scroll functions
-  $(".apt-body").on('click', (event) => {
-    console.log('clicked');
-    if ("#amenities" !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = "#amenities";
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, () => {
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    }
-  });
-  $("#amenities").on('click', (event) => {
-    console.log('clicked again');
-    if ("#interior" !== "") {
-      event.preventDefault();
-
-      var hash = "#interior";
-
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, () => {
-           window.location.hash = hash;
-      });
-    }
-  })
-  $("#interior").on('click', (event) => {
-    console.log('clicked thrice');
-    if ("#headline" !== "") {
-      event.preventDefault();
-
-      var hash = "#headline";
-
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800);
-    }
-  })
-
 // nav menu scroll downs
   $("#target1").on('click', (event) => {
     $("#hash1").toggle(600);
@@ -80,7 +32,7 @@ $(document).ready(() => {
   $("#label5").toggle(100);
  })
 
- //main-page section display
+ //main-page section scroll function
  $("#favorites-link").on('click', (e) => {
    $('.display-on-click').toggle(800);
     if (".display-on-click" !== ""){
@@ -92,20 +44,4 @@ $(document).ready(() => {
       })
     }
  });
-
 });
-
-
-
-
-// $(document).ready(function() {
-//   $('audio').mediaelementplayer({
-//       alwaysShowControls: true,
-//       features: ['playpause','volume', 'tracks', 'progress', 'duration', 'skipback'],
-//       audioVolume: 'horizontal',
-//       audioWidth: 325,
-//       audioHeight: 40,
-//       autoRewind: true,
-//       skipBackInterval: 3,
-//   }); 
-// })
